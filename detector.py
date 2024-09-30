f0b4f201e4d867e0d543f74f7bb2278d5eded9d7
unknown <markpekun379@gmail.com>
2024-10-01 02:17:56 +0300

    fix: bug in detection

--- detector.py
+++ detector.py
@@ -8,6 +8,7 @@ from typing import Any, Callable, List, Tuple
 import numpy as np
 import torch
 
+
 BASE_DIRECTORY = Path(__file__).parent
 YOLO_DIRECTORY = os.path.abspath(os.path.join(BASE_DIRECTORY))
 sys.path.append(YOLO_DIRECTORY)
@@ -26,7 +27,6 @@ class YoloDetector:
                  model_config=Path(BASE_DIRECTORY, "cfg/deploy/yolov7.yaml"), image_scaling=640):
         path_to_model = Path(BASE_DIRECTORY, model_file)
 
-
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
         if use_converted_model:
@@ -41,8 +41,8 @@ class YoloDetector:
         self.image_scaling = image_scaling
 
     def detect(self, image: np.ndarray, mapper: Callable = lambda x: x) -> Tuple[List[np.ndarray], List[Any], Any]:
-        img0 = letterbox(image, self.image_scaling, 32)[0]
-        img = img0[:, :, ::-1].transpose(2, 0, 1)
+        img0 = letterbox(image, self.image_scaling, 32)[0]  # Масштабированное изображение
+        img = img0[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB
         img = np.ascontiguousarray(img)
 
         img = torch.from_numpy(img).to(self.device)
@@ -60,10 +60,11 @@ class YoloDetector:
 
         for _, det in enumerate(predictions):
             if len(det):
+                # Используем размеры img0 для масштабирования координат
                 coords = scale_coords(img.shape[2:], det[:, :4], image.shape).round()
                 for coord, class_label in zip(coords, det[:, 5]):
-                    y0, x0, y1, x1 = coord
-                    img_labels_list.append(image[int(x0): int(y1), int(y0): int(x1)])
+                    y0, x0, y1, x1 = coord.int().tolist()  # Преобразуем в целые значения
+                    img_labels_list.append(image[int(y0): int(y1), int(x0): int(x1)])  # Изменено
                     detected_objects_coords.append(coord)
                     class_labels.append(mapper(class_label.item()))
         return img_labels_list, class_labels, detected_objects_coords
@@ -86,20 +87,20 @@ def CameraDetect():
         if ret:
             new_frame_time = cv2.getTickCount()
 
-            arr = detector1.detect(frame)
+            img_labels_list, class_labels, detected_objects_coords = detector1.detect(frame)
 
-            for i in range(len(arr[0])):
-                buff = arr[2][i].tolist()
+            for i in range(len(img_labels_list)):
+                buff = detected_objects_coords[i].tolist()
                 coords = [int(b) for b in buff]
-                cv2.rectangle(arr[0][i], (1, 1), (arr[0][i].shape[1] - 1, arr[0][i].shape[0] - 1),
-                              COLORS[int(arr[1][i])], 3)
-                cv2.putText(arr[0][i], OBJECTS[int(arr[1][i])], (10, 30), 3, 1, COLORS[int(arr[1][i])], 1, cv2.LINE_AA)
+                cv2.rectangle(frame, (coords[0], coords[1]), (coords[2], coords[3]), COLORS[int(class_labels[i])], 3)
+                cv2.putText(frame, OBJECTS[int(class_labels[i])], (coords[0], coords[1] - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, COLORS[int(class_labels[i])], 2, cv2.LINE_AA)
 
-            # Calculate FPS
+            # Рассчитать FPS
             fps = cv2.getTickFrequency() / (new_frame_time - prev_frame_time)
             prev_frame_time = new_frame_time
 
-            # Display FPS on the frame with floating point (e.g., 29.97)
+            # Отобразить FPS на кадре с плавающей точкой (например, 29.97)
             cv2.putText(frame, f"FPS: {fps:.2f}", (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2,
                         cv2.LINE_AA)
 
@@ -131,19 +132,15 @@ def VideoDetect(path: str):
     while cap.isOpened():
         ret, frame = cap.read()
         if ret:
-            if counter == 0:
-                arr = detector1.detect(frame)
+            arr = detector1.detect(frame)
 
-                for i in range(len(arr[0])):
-                    buff = arr[2][i].tolist()
-                    coords = [int(b) for b in buff]
-                    cv2.rectangle(arr[0][i], (1, 1), (arr[0][i].shape[1] - 1, arr[0][i].shape[0] - 1),
-                                  COLORS[int(arr[1][i])], 3)
-                    cv2.putText(arr[0][i], OBJECTS[int(arr[1][i])], (10, 30), 3, 1, COLORS[int(arr[1][i])], 1,
-                                cv2.LINE_AA)
+            for i in range(len(arr[0])):
+                buff = arr[2][i].tolist()
+                coords = [int(b) for b in buff]
+                cv2.rectangle(frame, (coords[0], coords[1]), (coords[2], coords[3]), COLORS[int(arr[1][i])], 3)
+                cv2.putText(frame, OBJECTS[int(arr[1][i])], (coords[0], coords[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, COLORS[int(arr[1][i])], 2, cv2.LINE_AA)
 
-                video_writer.write(frame)
-            counter = (counter + 1) % 5
+            video_writer.write(frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
@@ -153,5 +150,6 @@ def VideoDetect(path: str):
     cap.release()
     video_writer.release()
     cv2.destroyAllWindows()
+
 
 CameraDetect()
